7a194fa649bec8a9771cc58fa878907b6d16dd07
Daniel Lamando <apt@danopia.net>
2017-02-18 17:22:44 -0800

    turn assets back on

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module HeartbeatServer
 
 
 
-    config.assets.enabled = false
+    #config.assets.enabled = false
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
