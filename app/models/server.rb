b3f0ad7e954e321c623a06b76fc67c2564beebfa
Daniel Lamando <apt@danopia.net>
2017-02-18 17:04:26 -0800

    Upgrade Rails, 2.3.8 => 3.2

--- app/models/server.rb
+++ app/models/server.rb
@@ -2,11 +2,11 @@ class Server < ActiveRecord::Base
   validates_uniqueness_of :key
   validates_uniqueness_of :port, :scope => :hostname
   validates_presence_of :hostname, :port, :key
-  
-  named_scope :beating, lambda { { :conditions => ['updated_at > ?', 10.minutes.ago] } }
-  
+
+  scope :beating, lambda { { :conditions => ['updated_at > ?', 10.minutes.ago] } }
+
   # Server.dead.destroy
-  named_scope :dead, lambda { { :conditions => ['updated_at < ?', 1.month.ago] } } do
+  scope :dead, lambda { { :conditions => ['updated_at < ?', 1.month.ago] } } do
     def destroy
       each { |i| i.destroy }
     end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HeartbeatServer::Application.config.secret_token = '6588d122f89f359d88257b5264f0bd946c0a7d2034117533f2f765d0bcdef96848a3966e94a0e06ca8800a530b6b8e4b59355b2cd3f6b5e75a58aad44c307049'
